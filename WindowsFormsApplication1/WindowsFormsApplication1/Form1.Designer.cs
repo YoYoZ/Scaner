f9b82a4bf4a8b0772253f1f7dd7becbb5591dd51
Anton <yoyoza5@gmail.com>
2013-01-27 17:19:25 +0200

    First commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Step Two: Moving files";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 133);
+            this.progressBar1.MarqueeAnimationSpeed = 25;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(583, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(368, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 22);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(443, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(368, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Folder that you want to be output";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Folder to be proceed";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(215, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Start process";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 188);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(580, 23);
+            this.progressBar2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Step One: Scanning files";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(12, 240);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(580, 23);
+            this.progressBar3.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(150, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Step Three: Deleting duplicats";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 308);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Security.Cryptography;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public static volatile int percentOfCoping = 0;
+        public static volatile int percentOfDeleting = 0;
+        private FileScanner fs;
+        private Thread scanner = null;
+        private Thread copyrighter = null;
+        private Thread deleter = null;
+        private List<string[]> foundedFiles = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.MarqueeAnimationSpeed = 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            label2.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            folderBrowserDialog2.ShowDialog();
+            label3.Text = folderBrowserDialog2.SelectedPath;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            percentOfCoping = 0;
+            percentOfDeleting = 0;
+            button3.Enabled = false;
+            Scan_files();
+        }
+        private void Scan_files()
+        {
+            progressBar1.MarqueeAnimationSpeed = 25;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            button3.Enabled = false;
+            fs = new FileScanner(label2.Text);
+            scanner = new Thread(fs.scanFiles);
+            scanner.Start();
+        }
+        private void Copy_files()
+        {
+            int i = 1;
+            int g = 1;
+            for (int j = 0; j < foundedFiles.Count; j++)
+            {
+                try
+                {
+                    File.Copy(foundedFiles[j][1], label3.Text + "\\" + foundedFiles[j][0]);
+                }
+                catch (Exception exc)
+                {
+                    File.Copy(foundedFiles[j][1], label3.Text + "\\(" + i + ")" + g +")" +foundedFiles[j][0]);
+                    i++;
+                }
+                Form1.percentOfCoping = (j*100) / foundedFiles.Count;
+            }
+        } 
+
+        private void Deduplicate_Files()
+        {
+            percentOfCoping = 100;
+            string[] fs1 = Directory.GetFiles(label3.Text);
+            for (int i = 0; i < fs1.Length; i++)
+            {
+                string file1 = fs1[i];
+                for (int j = 0; j < fs1.Length; j++)
+                {
+                    string file2 = fs1[j];
+                    if (File.Exists(file1) && File.Exists(file2))
+                    {
+                            if (!file1.Equals(file2))
+                            {
+                                string md1 = getMd5(file1);
+                                string md2 = getMd5(file2);
+                                if (md1.Equals(md2))
+                                {
+                                    File.Delete(file2);
+
+                                }
+                            }
+                    }
+                }
+                Form1.percentOfDeleting = (i * 100) / fs1.Length;
+            }
+            percentOfDeleting = 100;
+        }
+        private string getMd5(string path)
+        {
+            long miliseconds = DateTime.Now.Millisecond;
+            FileStream fs = new FileStream(path, FileMode.Open);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] bytes = md5.ComputeHash(fs);
+            fs.Close();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("x2"));
+            }
+            Debug.WriteLine(DateTime.Now.Millisecond - miliseconds);
+            return sb.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar2.Value = percentOfCoping;
+            progressBar3.Value = percentOfDeleting;
+            if (scanner != null && !scanner.IsAlive)
+            {
+
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.Value = 100;
+                foundedFiles = fs.getFiles();
+                scanner = null;
+                fs = null;
+                copyrighter = new Thread(this.Copy_files);
+                copyrighter.Start();
+            }
+            if (copyrighter != null && !copyrighter.IsAlive)
+            {
+                copyrighter = null;
+                deleter = new Thread(this.Deduplicate_Files);
+                deleter.Start();
+            }
+            if (deleter != null && !deleter.IsAlive)
+            {
+                deleter = null;
+                button3.Enabled = true;
+            }
+        }
+    }
+
+    public class FileScanner
+    {
+        //Место, где будут храниться файлы.
+        //Это могила.
+        private List<string[]> scannedFiles;
+        private string pathToScan;
+
+        public FileScanner(string pathToScan)
+        {
+            scannedFiles = new List<string[]>();
+            this.pathToScan = pathToScan;
+        }
+
+        public void scanFiles()
+        {
+            string[] allFilesS = Directory.GetFiles(pathToScan);
+            if (allFilesS.Length != 0)
+            {
+                foreach (string file in allFilesS)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    string name = fi.Name;
+                    string fullPath = fi.FullName;
+                    string[] fileInfo = new string[] { name, fullPath};
+                    scannedFiles.Add(fileInfo);
+                }
+            }
+            string[] st = Directory.GetDirectories(pathToScan);
+            for (int i = 0; i < st.Length; i++)
+            {
+                string[] DirectoriesInCurrentFolder = Directory.GetDirectories(st[i]);
+                string[] allFiles = Directory.GetFiles(st[i]);
+                if (allFiles.Length != 0)
+                {
+                    foreach (string file in allFiles)
+                    {
+                        FileInfo fi = new FileInfo(file);
+                        string name = fi.Name;
+                        string fullPath = fi.FullName;
+                        string[] fileInfo = new string[] { name, fullPath};
+                        scannedFiles.Add(fileInfo);
+                    }
+                }
+                if (DirectoriesInCurrentFolder.Length != 0)
+                {
+                    pathToScan = st[i];
+                    scanFiles();
+                }
+            }
+        }
+        public List<string[]> getFiles()
+        {
+            return scannedFiles;
+        }
+    }
+}
